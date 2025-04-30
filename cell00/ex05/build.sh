735438a3c3d143dee44c32f9ddfd89d3c6bd7e79
Tanat Lertchoomongkol <talertch@z1t4c3.42bangkok.com>
2025-04-30 15:51:27 +0700

    update buildsh :

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,4 +1,8 @@
-for folder in "$@"
-do 
-	mkdir "ex$folder"
-done
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    for folder in "$@"; do
+        mkdir "ex$folder"
+    done
+fi
+
